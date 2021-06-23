7d2e54f081a7f44048bb25fb18c4448908f03246
hyukjun <hyukjun@hyukjunui-MacBookAir.local>
2021-06-23 23:08:23 +0900

    textField Test

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwiftStudy' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+
+pod 'RxSwift'
+pod 'RxCocoa'
+
+  # Pods for SwiftStudy
+
+  target 'SwiftStudyTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SwiftStudyUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ SwiftStudy/App/Main/MainViewController.swift
@@ -0,0 +1,117 @@
+//
+//  MainViewController.swift
+//  SwiftStudy
+//
+//  Created by hyukjun on 2021/06/15.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var buttonBottomConstraints: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.settingStyle()
+        self.settingKeyboard()
+        
+        self.textField.delegate = self
+    }
+    
+    func settingStyle() {
+        self.view.backgroundColor = .black
+        
+        self.textField.placeholder = "숫자만 입력하세요."
+        self.textField.setLeftPaddingPoints(5)
+    }
+    
+    func settingKeyboard() {
+        let tap = UIGestureRecognizer.init(target: self, action: #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(tap)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    @objc func keyboardShow(notification: NSNotification) {
+        let notiInfo = notification.userInfo!
+        
+        //키보드 높이 가져오기 위해
+        let keyboardFrame = notiInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        let height = keyboardFrame.size.height - self.view.safeAreaInsets.bottom
+        buttonBottomConstraints.constant = height
+        let animationDuration = notiInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+        
+        // 애니메이션 효과를 키보드 애니메이션 시간과 동일하게
+        UIView.animate(withDuration: animationDuration) {
+            self.buttonBottomConstraints.constant = height
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardHide(notification: NSNotification) {
+        
+        
+        let notiInfo = notification.userInfo!
+        //키보드 높이 가져오기 위해
+        let animationDuration = notiInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+        
+        // 애니메이션 효과를 키보드 애니메이션 시간과 동일하게
+        UIView.animate(withDuration: animationDuration) {
+            self.buttonBottomConstraints.constant = 0
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // self.inputField.resignFirstResponder()
+        // -> inputField 컨트롤에서 사용시 키패드 닫힘
+        // self.inputField.endEditing(true)
+        // -> inputField 컨트롤에서 사용시 키패드 닫힘
+        self.textField.endEditing(true)
+    }
+    
+    @IBAction func onClickButton(_ sender: Any) {
+        self.textField.endEditing(true)
+    }
+    
+}
+
+
+extension MainViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        /*
+         현재 커서 위치 : range.location
+         지워질 양 : range.length
+         추가될 값 : string
+         */
+        
+        guard let textFieldCount = textField.text?.count else { return false }
+        let textCount = textFieldCount + string.count - range.length
+        return !(textCount > 4)
+    }
+}
+
+extension UITextField {
+    func setLeftPaddingPoints(_ value:CGFloat){
+        //왼쪽에 여백 주기
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: value, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setRightPaddingPoints(_ value:CGFloat) {
+        //오른쪽에 여백 주기
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: value, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
